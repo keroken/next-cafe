386bdeee9b5210d049746218ec6681a09194fb48
keroken <kenichi.shinagawa@gmail.com>
2022-03-26 16:26:32 +0900

    base project

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/Footer.module.css'
+
+const Footer = () => (
+  <div className={styles.container}>
+    <h3 className={styles.title}>Next Cafe</h3>
+    <p className={styles.description}>cafe for your next visit</p>
+  </div>
+);
+
+export default Footer
